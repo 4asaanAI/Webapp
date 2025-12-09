989757d651e8b9d2c4b8b7bb0c9de0a381ddf8df
4asaanAI <aasaanai420@gmail.com>
2025-12-09 14:56:59 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col md:flex-row justify-between items-center gap-6">
           <div className="flex items-center gap-2">
-            <img className="w-12 h-12" src="public/logo1.png"/>
+            <img className="w-20 h-20" src="public/logo1.png"/>
           </div>
 
           <div className="text-center md:text-left">
